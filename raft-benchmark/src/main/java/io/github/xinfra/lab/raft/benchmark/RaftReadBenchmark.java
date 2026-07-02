0f4cfd4d478e64e8267b4f72b3a24814902eab08
joecqupt <joe469391363@gmail.com>
2026-07-02 23:44:37 +0800

    style: replace star imports with explicit JMH annotation imports

--- raft-benchmark/src/main/java/io/github/xinfra/lab/raft/benchmark/RaftReadBenchmark.java
+++ raft-benchmark/src/main/java/io/github/xinfra/lab/raft/benchmark/RaftReadBenchmark.java
@@ -8,7 +8,20 @@ package io.github.xinfra.lab.raft.benchmark;
 
 import io.github.xinfra.lab.raft.examples.KvServer;
 import io.github.xinfra.lab.raft.examples.proto.KvCommand;
-import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.annotations.AuxCounters;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.TearDown;
+import org.openjdk.jmh.annotations.Threads;
+import org.openjdk.jmh.annotations.Warmup;
 
 import java.io.IOException;
 import java.nio.file.Files;
